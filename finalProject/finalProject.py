2735a2d001eca692b7d78a17626c1fa69b8a1796
John Laine <john.laine5896@gmail.com>
2016-09-30 14:39:04 -0700

    feat: Add Jinja2 templating to all paths

--- finalProject/finalProject.py
+++ finalProject/finalProject.py
@@ -1,40 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
-	return 'This page will show all the restaurants'
+	return render_template('restaurants.html', restaurants = restaurants)
 
 @app.route('/restaurant/add')
 def addRestaurant():
-	return 'This page will contain a form to create a new restaurant'
+	return render_template('addRestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-	return 'This page will contain a form to edit a restaurant. restaurant_id: {}'.format(restaurant_id)
+	return render_template('editRestaurant.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-	return 'This page will contain a form (probably just a button really) to delete a restaurant. restaurant_id: {}'.format(restaurant_id)
+	return render_template('deleteRestaurant.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showRestaurantMenu(restaurant_id):
-	return 'This page will show a restaurant menu. restaurant_id: {}'.format(restaurant_id)
+	return render_template('menu.html', menu_items = items, restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/add')
 def addMenuItem(restaurant_id):
-	return 'This page will contain a form to add a menu item. restaurant_id: {}'.format(restaurant_id)
+	return render_template('addMenuItem.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_item_id>/edit')
 def editMenuItem(restaurant_id, menu_item_id):
-	return 'This page will contain a form to edit a menu item. restaurant_id: {}, menu_item_id: {}'.format(restaurant_id, menu_item_id)
+	return render_template('editMenuItem.html', restaurant = restaurant, menu_item = item)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_item_id>/delete')
 def deleteMenuItem(restaurant_id, menu_item_id):
-	return 'This page will contain a form to delete a menu item. restaurant_id: {}, menu_item_id: {}'.format(restaurant_id, menu_item_id)
+	return render_template('deleteMenuItem.html', restaurant = restaurant, menu_item = item)
 
 
 if __name__ == '__main__':
