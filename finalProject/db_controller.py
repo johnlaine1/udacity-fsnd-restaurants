3423566f15d0e18c6f37f8a5ad8692543e7cbe0f
John Laine <john.laine5896@gmail.com>
2016-10-01 06:56:06 -0700

    feat: Add CRUD functionality to final project app

---
+++ finalProject/db_controller.py
@@ -0,0 +1,72 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Restaurant, Base, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+def get_restaurants():
+	""" Get all restaurants """
+	restaurants = session.query(Restaurant).all()
+	return restaurants
+
+def create_restaurant(name):
+	restaurant = Restaurant(name = name)
+	session.add(restaurant)
+	session.commit()
+
+def get_restaurant(id):
+	restaurant = session.query(Restaurant).filter(Restaurant.id == id).one()
+	return restaurant
+
+def update_restaurant(id, name):
+	restaurant = get_restaurant(id)
+	restaurant.name = name
+	session.add(restaurant)
+	session.commit()
+
+def delete_restaurant(id):
+	restaurant = get_restaurant(id)
+	session.delete(restaurant)
+	session.commit()
+
+def get_menu_items_by_restaurant(restaurant_id):
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+	return items
+
+def get_menu_item(menu_item_id):
+	item = session.query(MenuItem).filter_by(id = menu_item_id).one()
+	return item
+
+def create_menu_item(name, description, price, course, restaurant_id):
+	item = MenuItem(name = name, description = description, price = price, course = course, restaurant_id = restaurant_id)
+	session.add(item)
+	session.commit()
+
+def update_menu_item(menu_item_id, name, description, price, course):
+	item = session.query(MenuItem).filter_by(id = menu_item_id).one()
+	item.name = name
+	item.description = description
+	item.price = price
+	item.course = course
+	session.add(item)
+	session.commit()
+
+def delete_menu_item(menu_item_id):
+	item = session.query(MenuItem).filter_by(id = menu_item_id).one()
+	session.delete(item)
+	session.commit()
--- finalProject/finalProject.py
+++ finalProject/finalProject.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for, request
+import db_controller
 
 #Fake Restaurants
 restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
@@ -13,39 +13,83 @@ item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/restaurants')
+@app.route('/restaurants/')
 def showRestaurants():
+	restaurants = db_controller.get_restaurants()
 	return render_template('restaurants.html', restaurants = restaurants)
 
-@app.route('/restaurant/add')
+@app.route('/restaurant/add/', methods = ['GET', 'POST'])
 def addRestaurant():
-	return render_template('addRestaurant.html')
+	if request.method == 'GET':
+		return render_template('addRestaurant.html')
+	if request.method == 'POST':
+		db_controller.create_restaurant(request.form['name'])
+		return redirect(url_for('showRestaurants'))
 
-@app.route('/restaurant/<int:restaurant_id>/edit')
+@app.route('/restaurant/<int:restaurant_id>/edit/', methods = ['GET', 'POST'])
 def editRestaurant(restaurant_id):
-	return render_template('editRestaurant.html', restaurant = restaurant)
+	restaurant = db_controller.get_restaurant(restaurant_id)
+	if request.method == 'GET':
+		return render_template('editRestaurant.html', restaurant = restaurant)
+	if request.method == 'POST':
+		db_controller.update_restaurant(restaurant.id, request.form['name'])
+		return redirect(url_for('showRestaurants'))
 
-@app.route('/restaurant/<int:restaurant_id>/delete')
+@app.route('/restaurant/<int:restaurant_id>/delete/', methods = ['GET', 'POST'])
 def deleteRestaurant(restaurant_id):
-	return render_template('deleteRestaurant.html', restaurant = restaurant)
+	restaurant = db_controller.get_restaurant(restaurant_id)
+	if request.method == 'GET':
+		return render_template('deleteRestaurant.html', restaurant = restaurant)
+	if request.method == 'POST':
+		db_controller.delete_restaurant(restaurant.id)
+		return redirect(url_for('showRestaurants'))
 
-@app.route('/restaurant/<int:restaurant_id>')
-@app.route('/restaurant/<int:restaurant_id>/menu')
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu/')
 def showRestaurantMenu(restaurant_id):
-	return render_template('menu.html', menu_items = items, restaurant = restaurant)
+	restaurant = db_controller.get_restaurant(restaurant_id)
+	menu_items = db_controller.get_menu_items_by_restaurant(restaurant_id)
+	return render_template('menu.html', menu_items = menu_items, restaurant = restaurant)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/add')
+@app.route('/restaurant/<int:restaurant_id>/menu/add/', methods = ['GET', 'POST'])
 def addMenuItem(restaurant_id):
-	return render_template('addMenuItem.html', restaurant = restaurant)
+	if request.method == 'GET':
+		return render_template('addMenuItem.html', restaurant = restaurant)
+	if request.method == 'POST':
+		db_controller.create_menu_item(
+			name = request.form['name'],
+			description = request.form['description'],
+			price = request.form['price'],
+			course = request.form['course'],
+			restaurant_id = restaurant_id)
+		return redirect(url_for('showRestaurantMenu', restaurant_id = restaurant_id))
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_item_id>/edit')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_item_id>/edit/', methods = ['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_item_id):
-	return render_template('editMenuItem.html', restaurant = restaurant, menu_item = item)
+	menu_item = db_controller.get_menu_item(menu_item_id)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_item_id>/delete')
+	if request.method == 'GET':
+		return render_template('editMenuItem.html', restaurant = restaurant, menu_item = menu_item)
+	if request.method == 'POST':
+		db_controller.update_menu_item(
+			menu_item_id = menu_item_id, 
+			name = request.form['name'],
+			description = request.form['description'],
+			price = request.form['price'],
+			course = request.form['course'])
+		return redirect(url_for('showRestaurantMenu', restaurant_id = restaurant_id))
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_item_id>/delete/', methods = ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_item_id):
-	return render_template('deleteMenuItem.html', restaurant = restaurant, menu_item = item)
+	restaurant = db_controller.get_restaurant(restaurant_id)
+	menu_item = db_controller.get_menu_item(menu_item_id)
 
+	if request.method == 'GET':
+		return render_template('deleteMenuItem.html', restaurant = restaurant, menu_item = menu_item)
+
+	if request.method == 'POST':
+		db_controller.delete_menu_item(menu_item_id)
+		return redirect(url_for('showRestaurantMenu', restaurant_id = restaurant_id))
 
 if __name__ == '__main__':
 	app.debug = True
